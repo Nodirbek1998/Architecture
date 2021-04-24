a6267af711cbc395c5be4903dc95aacdb54d04a1
user <nodirbekmamadaliyev1998@gmail.com>
2021-04-24 18:52:38 +0500

    architecture

---
+++ controllersestem/src/main/java/uz/cas/controllersestem/entity/Document.java
@@ -0,0 +1,17 @@
+package uz.cas.controllersestem.entity;
+
+
+
+import javax.persistence.*;
+
+
+@Entity
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+}
